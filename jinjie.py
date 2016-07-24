63c09fa7bb76317e355ab10cafcddbb4607ef617
knoixs <knoixs@gmail.com>
2016-07-25 06:58:58 +0800

    2-6

--- jinjie.py
+++ jinjie.py
@@ -6,3 +6,19 @@
 #
 # print (add(25,9,math.sqrt))
 
+
+# #2-4
+# def format_name(s):
+#     return s.capitalize()
+#
+# print map(format_name, ['adam', 'LISA', 'barT'])
+
+
+# #2-6
+# import math
+# 
+# def is_sqr(x):
+#     r = int(math.sqrt(x))
+#     return r*r==x
+#
+# print filter(is_sqr, range(1, 101))
