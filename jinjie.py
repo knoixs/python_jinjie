039a4eb40a7d4909e7ef27c806284557cdea7bcf
knoixs <knoixs@gmail.com>
2016-07-27 15:41:38 +0800

    2-6至2-8

--- jinjie.py
+++ jinjie.py
@@ -16,9 +16,51 @@
 
 # #2-6
 # import math
-# 
+#
 # def is_sqr(x):
 #     r = int(math.sqrt(x))
 #     return r*r==x
 #
 # print filter(is_sqr, range(1, 101))
+
+
+# #2-7
+# def cmp_ignore_case(s1,s2):
+#     if s1.lower()>s2.lower():
+#         return 1
+#     if s1.lower()<s2.lower():
+#         return -1
+#     return 0
+#
+# print sorted(['bob', 'about', 'Zoo', 'Credit'], cmp_ignore_case)
+
+# print ('Hi,{name},{message}'.format(name='Tom',message='hihao'))
+
+
+# def new_counter(n):
+#
+#     class Counter(object):
+#         def __init__(self, num):
+#             self.num =num
+#
+#         def __call__(self):
+#             self.num += 1
+#             return self.num
+#
+#     return Counter(n)
+#
+# c1 = new_counter(10)
+# c2 = new_counter(20)
+# print c1(), c2(), c1(), c2()
+
+
+#2-8
+def calc_prod(lst):
+    def lazy_prod(x,y):
+        return x*y
+    def prod():
+        return reduce(lazy_prod,lst)
+    return prod
+
+f = calc_prod([1, 2, 3, 4])
+print f()
