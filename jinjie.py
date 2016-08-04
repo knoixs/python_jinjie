f412215a9063f2d5a83f9576ac0a695cb33fc1f8
knoixs <knoixs@gmail.com>
2016-08-04 15:48:45 +0800

    2-8至2-12

--- jinjie.py
+++ jinjie.py
@@ -154,3 +154,17 @@
 #     return reduce(lambda x,y: x*y, range(1, n+1))
 # print factorial(10)
 
+# import time
+# def performance(f):
+#     def fn(*args,**kwargs):
+#         t1=time.time()
+#         r=f(*args,**kwargs)
+#         t2=time.time()
+#         print 'call %s() in %fs'%(f.__name__,(t2-t1))
+#         return r
+#     return fn
+#
+# @performance
+# def factorial(n):
+#     return reduce(lambda x,y:x*y,range(1,n+1))
+# print factorial(10)
